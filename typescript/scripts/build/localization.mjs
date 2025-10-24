c827e10b41ff752cc35f3f920d58a98dbcee82ce
cavassani <lucas.cavassani.q@gmail.com>
2025-10-24 10:24:35 -0300

    adição de alguns arquivos no diretório de `build` dos scripts do typescript

---
+++ typescript/scripts/build/localization.mjs
@@ -0,0 +1,15 @@
+export const localizationDirectories = [
+    "cs",
+    "de",
+    "es",
+    "fr",
+    "it",
+    "ja",
+    "ko",
+    "pl",
+    "pt-br",
+    "ru",
+    "tr",
+    "zh-cn",
+    "zh-tw"
+].map(f => f.toLowerCase());
