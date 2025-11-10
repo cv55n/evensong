96bb9376fdae6b1f4cc49a78b5dd3a425c68167e
cavassani <lucas.cavassani.q@gmail.com>
2025-11-10 10:25:16 -0300

    `getfileinfo` corrigido

---
+++ prettier/src/cli/file-info.js
@@ -0,0 +1,32 @@
+import path from "node:path";
+import stringify from "fast-json-stable-stringify";
+
+import { format, getFileInfo } from "../index.js";
+import { printToScreen } from "./utils.js";
+
+// nota: isso não funciona com `--config-precedence`
+async function logFileInfoOrDie(context) {
+    const {
+        fileInfo: file,
+        ignorePath,
+        withNodeModules,
+        plugins,
+        config
+    } = context.argv;
+
+    const fileInfo = await getFileInfo(path.resolve(file), {
+        ignorePath,
+        withNodeModules,
+        plugins,
+
+        resolveConfig: config !== false
+    });
+
+    const result = await format(stringify(fileInfo), {
+        parser: "json"
+    });
+
+    printToScreen(result.trim());
+}
+
+export default logFileInfoOrDie;
