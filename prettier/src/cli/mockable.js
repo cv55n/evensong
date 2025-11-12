f102490601371dbd572dde42b55698ffdc56f8bb
cavassani <lucas.cavassani.q@gmail.com>
2025-11-12 11:31:45 -0300

    refatoração das funcionalidades simuláveis

---
+++ prettier/src/cli/mockable.js
@@ -0,0 +1,23 @@
+import { performance } from "node:perf_hooks";
+import { isCI } from "ci-info";
+
+import fs from "node:fs/promises";
+
+// @ts-expect-error
+import { __internal as sharedWithCli } from "../index.js";
+
+import clearStreamText from "./utilities/clear-stream-text.js";
+
+const mockable = sharedWithCli.utils.createMockable({
+    clearStreamText,
+
+    getTimestamp: performance.now.bind(performance),
+
+    isCI: () => isCI,
+    isStreamTTY: (stream) => stream.isTTY,
+    writeFormattedFile: (file, data) => fs.writeFile(file, data)
+});
+
+export default mockable.mocked;
+
+export { mockable };
