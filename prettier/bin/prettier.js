156e1d3a98afde309957c66bee2109d784f41c50
cavassani <lucas.cavassani.q@gmail.com>
2025-11-12 10:44:36 -0300

    utilizar `@prettier/next`

---
+++ prettier/bin/prettier.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/*
+adicione este arquivo para que possamos usar `node bin/prettier`
+ou `node bin/prettier.js` em vez de `node bin/prettier.cjs`
+
+este arquivo deve ser usado apenas para desenvolvimento
+*/
+
+import "./prettier.cjs";
