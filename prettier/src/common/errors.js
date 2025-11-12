bae3a7f2d2861d7eb84e9d49b4ae636455c1e2f6
cavassani <lucas.cavassani.q@gmail.com>
2025-11-12 11:14:16 -0300

    adição de `eslint-plugin-simple-import-sort`

---
+++ prettier/src/common/errors.js
@@ -0,0 +1,17 @@
+class ConfigError extends Error {
+    name = "ConfigError";
+}
+
+class UndefinedParserError extends Error {
+    name = "UndefinedParserError";
+}
+
+class ArgExpansionBailout extends Error {
+    name = "ArgExpansionBailout";
+}
+
+export {
+    ArgExpansionBailout,
+    ConfigError,
+    UndefinedParserError
+};
