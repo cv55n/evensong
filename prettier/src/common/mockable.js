7049b5bd684cbf51e5c2b3a6518081fa84c3d148
cavassani <lucas.cavassani.q@gmail.com>
2025-11-12 11:20:59 -0300

    refatoração das funcionalidades simuláveis

---
+++ prettier/src/common/mockable.js
@@ -0,0 +1,9 @@
+import createMockable from "../utils/create-mockable.js";
+
+const mockable = createMockable({
+    getPrettierConfigSearchStopDirectory: () => undefined,
+});
+
+export default mockable.mocked;
+
+export { mockable };
