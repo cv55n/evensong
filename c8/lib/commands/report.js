a3bb2b257560b35ed1909805e8e3db3ca861e851
cavassani <lucas.cavassani.q@gmail.com>
2025-11-17 15:36:25 -0300

    adição de reports experimentais do monocart

---
+++ c8/lib/commands/report.js
@@ -0,0 +1,52 @@
+const { checkCoverages } = require('./check-coverage');
+
+const Report = require('../report');
+
+exports.command = 'report';
+exports.describe = 'lê os dados de cobertura do v8 a partir do relatório temporário e de saída';
+
+exports.handler = async function(argv) {
+    await exports.outputReport(argv);
+}
+
+exports.outputReport = async function(argv) {
+    // todo: esta é uma solução alternativa até que o yargs
+    // seja atualizado para a versão 17. veja:
+    //
+    // https://github.com/bcoe/c8/pull/332#discussion_r721636191
+
+    if (argv['100']) {
+        argv.checkCoverage = 100;
+        argv.lines = 100;
+        argv.functions = 100;
+        argv.branches = 100;
+        argv.statements = 100;
+    }
+
+    const report = Report({
+        include: argv.include,
+        exclude: argv.exclude,
+        extension: argv.extension,
+        excludeAfterRemap: argv.excludeAfterRemap,
+        reporter: Array.isArray(argv.reporter) ? argv.reporter : [argv.reporter],
+        reportsDirectory: argv['reports-dir'],
+        reporterOptions: argv.reporterOptions || {},
+        tempDirectory: argv.tempDirectory,
+        watermarks: argv.watermarks,
+        resolve: argv.resolve,
+        omitRelative: argv.omitRelative,
+        wrapperLength: argv.wrapperLength,
+        all: argv.all,
+        allowExternal: argv.allowExternal,
+        src: argv.src,
+        skipFull: argv.skipFull,
+        excludeNodeModules: argv.excludeNodeModules,
+        mergeAsync: argv.mergeAsync,
+        monocartArgv: (argv.experimentalMonocart || process.env.EXPERIMENTAL_MONOCART) ? argv : null
+    });
+
+    await report.run();
+
+    if (argv.checkCoverage)
+        await checkCoverages(argv, report);
+}
