a40ecd5bbad69f62d2cd220b16f9edc711bf666f
cavassani <lucas.cavassani.q@gmail.com>
2025-11-24 10:21:46 -0300

    adição da funcionalidade `--all`

---
+++ c8/test/fixtures/all/ts-compiled/dir/unloaded.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.__esModule = true;
+
+function Unloaded() {
+	return 'nunca carregado :(';
+}
+
+exports["default"] = Unloaded;
+
+console.log("esse arquivo não deveria ter sido avaliado");
+
+// # sourcemappingurl = unloaded.js.map
---
+++ c8/test/fixtures/all/ts-compiled/dir/unloaded.ts
@@ -0,0 +1,5 @@
+export default function Unloaded() {
+	return 'nunca carregado :(';
+}
+
+console.log("esse arquivo não deveria ter sido avaliado.");
