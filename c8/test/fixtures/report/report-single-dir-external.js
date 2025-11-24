0e8c83cc04206e8ed3eb9a1b96117da2e388a4b8
cavassani <lucas.cavassani.q@gmail.com>
2025-11-24 13:20:13 -0300

    suporte para instrumentação de arquivos fora do diretório de trabalho atual

---
+++ c8/test/fixtures/report/report-single-dir-external.js
@@ -0,0 +1,18 @@
+const Report = require('../../../lib/report');
+
+const report = new Report({
+	include: ['**/*.js'],
+	exclude: [],
+	reporter: ['text'],
+
+	tempDirectory: './temp',
+
+	omitRelative: true,
+	all: true,
+
+	src: '../multidir1/',
+	
+	allowExternal: true
+});
+
+report.run();
