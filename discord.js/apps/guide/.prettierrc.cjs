df13fa8804de9768f6f65584dc4a387fdf45116e
cavassani <lucas.cavassani.q@gmail.com>
2025-11-26 15:24:46 -0300

    docs: configuração e setup de guia

---
+++ discord.js/apps/guide/.prettierrc.cjs
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+    ...require('../../.prettierrc.json'),
+
+    plugins: [
+        'prettier-plugin-tailwindcss'
+    ],
+
+    tailwindFunctions: [
+        'cva',
+        'cx'
+    ]
+};
