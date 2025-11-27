1dc083f3fdf315474c7a503c0f124f9fc537e462
cavassani <lucas.cavassani.q@gmail.com>
2025-11-27 11:00:04 -0300

    correção no comportamento da barra lateral ao alterar entre pacotes/versões

---
+++ discord.js/apps/website/src/app/api/docs/entrypoints/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+import { fetchEntryPoints } from '@/util/fetchEntryPoints';
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = request.nextUrl;
+
+    const packageName = searchParams.get('packageName');
+    const version = searchParams.get('version');
+
+    if (!packageName || !version) {
+        return NextResponse.json({
+            error: 'parâmetros necessários ausentes'
+        }, {
+            status: 400
+        });
+    }
+
+    const response = await fetchEntryPoints(packageName, version);
+
+    return NextResponse.json(response);
+}
