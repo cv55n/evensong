e06a51dfb2dd7b847ef3762983dffb4e9e06e848
cavassani <lucas.cavassani.q@gmail.com>
2025-12-09 09:54:05 -0300

    adição apropriada de diversas configurações

---
+++ discord.js/packages/core/tsup.config.ts
@@ -0,0 +1,17 @@
+import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
+
+import { createTsupConfig } from '../../tsup.config.js';
+
+export default [
+    createTsupConfig({
+        esbuildPlugins: [esbuildPluginVersionInjector()]
+    }),
+
+    createTsupConfig({
+        entry: {
+            'http-only': 'src/http-only/index.ts'
+        },
+
+        esbuildPlugins: [esbuildPluginVersionInjector()]
+    })
+];
